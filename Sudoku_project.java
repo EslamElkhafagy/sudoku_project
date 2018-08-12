d5be8d0ab5b08efb441f5520d80a5634b1ff88a8
EslamElkhafagy <eslamelkhafagy@gmail.com>
2018-08-12 13:35:38 +0200

    first commit

---
+++ Sudoku_project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku_project;
+
+/**
+ *
+ * @author Enjoy
+ */
+public class Sudoku_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
